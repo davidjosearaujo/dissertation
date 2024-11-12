c1dcfdbe4f3c808067a92c0b311c984a0e1f43c2
David Araújo <david2araujo5@gmail.com>
2024-11-12 12:03:46 +0000

    Removed thesis template

--- thesis/build.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm -v "$(pwd)":/home --entrypoint=/bin/bash debian -c "
-    apt update && \
-    apt install -y make lsb-release && \
-    cd /home && \
-    make setup && \
-    make && \ 
-    make print"
